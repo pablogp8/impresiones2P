a8f8c90d12f7153dceb058e4a1d9005b7b32f7c3
pablogp8 <pablo.ys@hotmail.com>
2019-02-27 09:59:42 -0600

    Creacion de componente solicitud

---
+++ src/app/solicitud-p/solicitud-p.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud-p',
+  templateUrl: './solicitud-p.component.html',
+  styleUrls: ['./solicitud-p.component.css']
+})
+export class SolicitudPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
